20843ece1cb00bb47356e24e021ef873fa90b7aa
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-03-20 10:29:58 -0300

    abilities setup

---
+++ Source/MyGame/Abilities/MyGameplayAbility.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "MyGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGAME_API UMyGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
--- Source/MyGame/MyGameGameMode.cpp
+++ Source/MyGame/MyGameGameMode.cpp
@@ -7,7 +7,8 @@
 AMyGameGameMode::AMyGameGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_Char"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
