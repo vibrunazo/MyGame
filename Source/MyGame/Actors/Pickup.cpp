705121dc44c406b736c93d7e3681cc55d52e705f
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-04-27 06:44:36 -0300

    pickups

---
+++ Source/MyGame/Actors/Pickup.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+APickup::APickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// PrimaryActorTick.bCanEverTick = true;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = RootComp;
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	BoxCollision->SetupAttachment(RootComponent);
+	BoxCollision->SetBoxExtent(FVector(50.f, 50.f, 50.f));
+	OnActorBeginOverlap.AddDynamic(this, &APickup::OnPickupBeginOverlap);
+
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APickup::OnPickupBeginOverlap(AActor* OverlappingActor, AActor* OtherActor)
+{
+	UE_LOG(LogTemp, Warning, TEXT("%s Overlapped %s"), *OverlappingActor->GetName(), *OtherActor->GetName());
+}
---
+++ Source/MyGame/Actors/Pickup.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class MYGAME_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+	UFUNCTION()
+	void OnPickupBeginOverlap(AActor* OverlappingActor, AActor* OtherActor);
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Pickup)
+	class USceneComponent* RootComp;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Pickup)
+	class UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Pickup)
+	class UBoxComponent* BoxCollision;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
