47baadfa1d43b82baac5f2775ee64f563b5d003c
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-05-18 23:54:55 -0300

    room camera pawn adjustable settings

--- Source/MyGame/Level/RoomCameraPawn.cpp
+++ Source/MyGame/Level/RoomCameraPawn.cpp
@@ -2,12 +2,42 @@
 
 
 #include "RoomCameraPawn.h"
+#include "../Player/MyCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
 ARoomCameraPawn::ARoomCameraPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MovementComponent"));
+	MovementComponent->ProjectileGravityScale = 0.f;
+	MovementComponent->bIsHomingProjectile = true;
+	MovementComponent->HomingAccelerationMagnitude = 10.f;
+	MovementComponent->HomingTargetComponent;
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = RootComp;
+	// CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	// CameraBoom->SetupAttachment(RootComponent);
+	// CameraBoom->TargetArmLength = 700.0f; // The camera follows at this distance behind the character	
+	// CameraBoom->SetWorldRotation(FRotator(-40.0f, 0.0f, 0.0f));
+	// CameraBoom->bInheritYaw = false;
+	// CameraBoom->bInheritPitch = false;
+	// CameraBoom->bInheritRoll = false;
+	// CameraBoom->bDoCollisionTest = false;
+	// CameraBoom->bEnableCameraLag = true;
+	// CameraBoom->CameraLagSpeed = 2.0f;
+	// CameraBoom->CameraLagMaxDistance = 200.0f;
+	// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(RootComponent);
+	// FollowCamera->SetRelativeLocation(FVector(0.f, 0.f, 500.f));
+	// FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 }
 
@@ -18,17 +48,40 @@ void ARoomCameraPawn::BeginPlay()
 	
 }
 
-// Called every frame
 void ARoomCameraPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (PlayerRef)
+	{
+		FollowPlayer();
+	}
+
 }
 
-// Called to bind functionality to input
-void ARoomCameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ARoomCameraPawn::SetPlayerRef(AMyCharacter* NewPlayer)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerRef = NewPlayer;
+	// MovementComponent->HomingTargetComponent = PlayerRef->GetRootComponent();
 }
 
+void ARoomCameraPawn::FollowPlayer()
+{
+	// FollowCamera->RelativeRotation(NewRot);
+	float TargetX = PlayerRef->GetActorLocation().X ;
+	// Target = PlayerRef->GetActorLocation()  + FVector(-800.f - TargetX * 0.8, 0.f, 300.f);
+	Target = PlayerRef->GetActorLocation()  + CameraDistance;
+	Target.X -= TargetX * XRatio;
+	GetActorLocation();
+	FVector CurLoc = FMath::Lerp(GetActorLocation(), Target, LerpSpeed);
+	// CurLoc += FVector(-500.f, 0.f, 400.f);
+
+	SetActorLocation(CurLoc);
+
+	ViewTarget = FMath::Lerp(ViewTarget, PlayerRef->GetActorLocation(), RotLerp);
+	FRotator NewRot = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), ViewTarget);
+	// FRotator CurRot = FMath::Lerp(GetActorRotation(), NewRot, 0.1f);
+	SetActorRotation(NewRot);
+	// SetActorLocation(FVector(GetActorLocation().X, Target.Y, GetActorLocation().Z));
+
+}
--- Source/MyGame/Level/RoomCameraPawn.h
+++ Source/MyGame/Level/RoomCameraPawn.h
@@ -14,16 +14,38 @@ class MYGAME_API ARoomCameraPawn : public APawn
 public:
 	// Sets default values for this pawn's properties
 	ARoomCameraPawn();
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable, Category = RoomCamera)
+	void SetPlayerRef(class AMyCharacter* NewPlayer);
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Camera)
+	class USceneComponent* RootComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	class UCameraComponent* FollowCamera;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	class UProjectileMovementComponent* MovementComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	class AMyCharacter* PlayerRef;
+	FVector Target = FVector();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	FVector CameraDistance = FVector(-800.f, 0.f, 300.f);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	float XRatio = 0.5f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	float LerpSpeed = 0.05f;
+	FVector ViewTarget = FVector();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Camera)
+	float RotLerp = 0.05f;
+
+private:
+	void FollowPlayer();
+	
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
