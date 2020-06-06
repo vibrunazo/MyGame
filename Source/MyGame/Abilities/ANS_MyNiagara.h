950065d8f3421e26c128a323fb6b403950667957
Vandré Brunazo <vibrunazo@gmail.com>
2020-06-05 23:28:13 -0300

    ANS Niagara child

---
+++ Source/MyGame/Abilities/ANS_MyNiagara.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimNotifyState_TimedNiagaraEffect.h"
+#include "ANS_MyNiagara.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGAME_API UANS_MyNiagara : public UAnimNotifyState_TimedNiagaraEffect
+{
+	GENERATED_BODY()
+	
+};
