cf602c5b6d1a3f82f7faf324690c44d71fca9f0a
Vandré Brunazo <vibrunazo@gmail.com>
2020-06-10 01:35:11 -0300

    update all anim hitboxes to new system

--- Source/MyGame/Abilities/ANS_Hitbox.cpp
+++ Source/MyGame/Abilities/ANS_Hitbox.cpp
@@ -20,12 +20,10 @@ void UANS_Hitbox::NotifyBegin(USkeletalMeshComponent* MeshComp, class UAnimSeque
 
 	if (!GetWorld())
 	{
-		UE_LOG(LogTemp, Warning, TEXT("I'm in editor"));
 		AActor* Owner = MeshComp->GetOwner();
 
 		if (!Owner) return;
 		FVector Loc = Owner->GetActorLocation();
-		UE_LOG(LogTemp, Warning, TEXT("Owner is: %s, is pawn: %d"), *Owner->GetName(), Cast<APawn>(Owner));
 		FActorSpawnParameters params;
 		params.bNoFail = true;
 		params.Instigator = (APawn*)(Owner);
@@ -41,18 +39,14 @@ void UANS_Hitbox::NotifyBegin(USkeletalMeshComponent* MeshComp, class UAnimSeque
 
 void UANS_Hitbox::NotifyEnd(USkeletalMeshComponent* MeshComp, class UAnimSequenceBase* Animation)
 {
-	UE_LOG(LogTemp, Warning, TEXT("ans hitbox notify end"));
-
 	TArray<USceneComponent*> Children;
 	MeshComp->GetChildrenComponents(false, Children);
 
 	for (USceneComponent* Component : Children)
 	{
-		
-		UE_LOG(LogTemp, Warning, TEXT("Found child %s of %s"), *Component->GetName(), *Component->GetOwner()->GetName());
+		if (!Component->GetOwner()) continue;
 		if (AHitBox* Hitbox = Cast<AHitBox>(Component->GetOwner()))
 		{
-			UE_LOG(LogTemp, Warning, TEXT("is hitbox"));
 			Hitbox->Destroy();
 		}
 	}
