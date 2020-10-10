452ee3ed8c42ff63b97d9dfa845252a8de427f25
Vandré Brunazo <vibrunazo@gmail.com>
2020-10-10 15:34:45 -0300

    fix warning trying to rotate simated mesh

--- Source/MyGame/Abilities/ANS_RotateChar.cpp
+++ Source/MyGame/Abilities/ANS_RotateChar.cpp
@@ -5,16 +5,18 @@
 
 void UANS_RotateChar::NotifyTick(class USkeletalMeshComponent* MeshComp, class UAnimSequenceBase* Animation, float FrameDeltaTime)
 {
+	if (MeshComp->IsSimulatingPhysics()) return;
 	FRotator CurrentRotation = RotationSpeed * FrameDeltaTime * 360.f;
 	FRotator LastRotation = MeshComp->GetRelativeRotation();
-	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime);
 	//UE_LOG(LogTemp, Warning, TEXT("Notify Ticking, last rot: %s"), *LastRotation.ToString());
 	MeshComp->AddLocalRotation(CurrentRotation);
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime);
 }
 
 
 void UANS_RotateChar::NotifyEnd(USkeletalMeshComponent* MeshComp, class UAnimSequenceBase* Animation)
 {
+	if (MeshComp->IsSimulatingPhysics()) return;
 	MeshComp->SetRelativeRotation(FRotator(0.f, 270.f, 0.f));
 	FRotator LastRotation = MeshComp->GetRelativeRotation();
 	//UE_LOG(LogTemp, Warning, TEXT("Notify Ending, last rot: %s"), *LastRotation.ToString());
